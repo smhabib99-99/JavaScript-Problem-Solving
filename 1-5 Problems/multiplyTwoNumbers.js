9c8580547342f83ef1375de09de61897d201ce6e
smhabib99-99 <habibaust34@gmail.com>
2024-05-14 18:50:45 +0600

    written a function to multiply two numbers in JavaScript

---
+++ 1-5 Problems/multiplyTwoNumbers.js
@@ -0,0 +1,12 @@
+// Writing a Function two multiply two numbers in JavaScript
+
+const x = 3;
+const y = 10;
+
+function multiplyTwoNumbers(num1,num2){
+    const mul = num1*num2;
+    return mul;
+}
+
+const solve = multiplyTwoNumbers(x,y);
+console.log(solve);
