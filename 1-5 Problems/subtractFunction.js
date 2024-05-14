f5ea6aa1ba056b017aa4794bb6d2bfd74b755498
smhabib99-99 <habibaust34@gmail.com>
2024-05-14 18:43:57 +0600

    Subtracting two numbers using js function and simple way

---
+++ 1-5 Problems/subtractFunction.js
@@ -0,0 +1,12 @@
+// Subtract Two numbers Using function in javaScript
+
+const x = 10;
+const y = 3;
+
+function subtractTwoNumbers(num1,num2){
+    const subtract = num1-num2;
+    return subtract;
+}
+
+const solution = subtractTwoNumbers(x,y);
+console.log(solution);
---
+++ 1-5 Problems/subtractNumbers.js
@@ -0,0 +1,8 @@
+// Subtract two numbers using Simple mathematical way
+
+const x = 10;
+const y = 2;
+
+const subtract = x-y;
+
+console.log(subtract);
