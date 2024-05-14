8f248a5fb15700d556ddc7efd89c934a71071466
smhabib99-99 <habibaust34@gmail.com>
2024-05-14 18:32:33 +0600

    Adding Two numbers in simple way and using function

---
+++ 1-5 Problems/addTwoNumbers.js
@@ -0,0 +1,10 @@
+// Add Two numbers simple way
+const x = 5
+const y = 10
+
+const sum = x+y
+
+console.log(sum)
+
+
+// if numbers is in string form then we should use parseInt
---
+++ 1-5 Problems/addTwoNumbersFunction.js
@@ -0,0 +1,12 @@
+// Adding Two Number using function in JavaScript
+const x = 3;
+const y = 10;
+
+function addTwoNumbers(num1,num2){
+    const sum = num1+num2;
+    return sum;
+}
+
+const output = addTwoNumbers(x,y);
+
+console.log(output);
