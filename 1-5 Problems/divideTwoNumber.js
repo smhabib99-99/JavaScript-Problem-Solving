486beac98931c037ac9753fa2654b7327d3a3a3c
smhabib99-99 <habibaust34@gmail.com>
2024-05-14 18:54:07 +0600

    writing a function to divide two numbers in JavaScript

---
+++ 1-5 Problems/divideTwoNumber.js
@@ -0,0 +1,12 @@
+// Dividing Two numbers using JavaScript Function
+
+const x = 33;
+const y = 3;
+
+function divideTwoNumbers(num1,num2){
+    const div = num1/num2;
+    return div;
+}
+
+const solve = divideTwoNumbers(x,y);
+console.log(solve);
