cf93cce826fb4fdfbff87fa601bcaae112236793
smhabib99-99 <habibaust34@gmail.com>
2024-05-15 17:03:12 +0600

    return two max number from an array

--- 6-10 Problems/twoMax.js
+++ 6-10 Problems/twoMax.js
@@ -2,12 +2,16 @@
  * writing a program from 5 numbers return 2 max number.
  */
 
-const num = [2,1,3,5,4];
+const num = [2, 1, 3, 5, 4];
 // const num = [];
+if (num.length != 0) {
+    num.sort(function (a, b) {
+        return b - a;
+    });
 
-num.sort(function(a,b){
-    return b-a;
-});
+    console.log(num[0], num[1]);
+}
+
 // const b = num.reverse();
-console.log(num[0],num[1]);
+// console.log(num[0], num[1]);
 // console.log(b[0],b[1]);
