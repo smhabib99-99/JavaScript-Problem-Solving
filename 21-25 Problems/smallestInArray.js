2201ddcb2251f8c719e894ed872ddcb81c2809f1
smhabib99-99 <habibaust34@gmail.com>
2024-05-25 13:18:42 +0600

    This code finds the smallest number in an array

--- 21-25 Problems/smallestInArray.js
+++ 21-25 Problems/smallestInArray.js
@@ -1,3 +1,16 @@
 /**
  * Write a function to find the smallest number in an array.
  */
+
+function getSmallestNumber(arr) {
+    let min = Number.MAX_VALUE;
+    for (let i = 0; i < arr.length; i++) {
+        if (min > arr[i]) {
+            min = arr[i];
+        }
+    }
+    return min;
+}
+const x = [1, 2, 3, 4, 5];
+const solve = getSmallestNumber(x);
+console.log(solve);
