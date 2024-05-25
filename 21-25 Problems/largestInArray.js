b00184cba1e25e99c01b0592b7158b8d43ecee24
smhabib99-99 <habibaust34@gmail.com>
2024-05-25 13:12:11 +0600

    This code finds the largest number in an array

--- 21-25 Problems/largestInArray.js
+++ 21-25 Problems/largestInArray.js
@@ -1,3 +1,16 @@
 /**
  * Write a function to find the largest number in an array.
  */
+
+function getLargestInArray(arr) {
+    let max = Number.MIN_VALUE;
+    for (let i = 0; i < arr.length; i++) {
+        if (max < arr[i]) {
+            max = arr[i];
+        }
+    }
+    return max;
+}
+const x = [1, 2, 3, 4, 5];
+const solve = getLargestInArray(x);
+console.log(solve);
